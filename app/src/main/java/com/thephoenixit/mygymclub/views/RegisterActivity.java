7419f653e14e73eea3936b5a4caa61924d1a33bb
hamzajg <hamzajg@gmail.com>
2018-01-14 04:52:42 +0900

    Work On User UI...

--- app/src/main/java/com/thephoenixit/mygymclub/views/RegisterActivity.java
+++ app/src/main/java/com/thephoenixit/mygymclub/views/RegisterActivity.java
@@ -1,7 +1,8 @@
 package com.thephoenixit.mygymclub.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import com.thephoenixit.mygymclub.R;
 
 public class RegisterActivity extends AppCompatActivity {
