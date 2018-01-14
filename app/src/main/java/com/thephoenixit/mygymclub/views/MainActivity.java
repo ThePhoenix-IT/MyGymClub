47df9580120d8e8c0d10008d51aac95346897467
hamzajg <hamzajg@gmail.com>
2018-01-14 21:21:13 +0900

    Work On Chart UI...

--- app/src/main/java/com/thephoenixit/mygymclub/views/MainActivity.java
+++ app/src/main/java/com/thephoenixit/mygymclub/views/MainActivity.java
@@ -1,5 +1,7 @@
 package com.thephoenixit.mygymclub.views;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -12,22 +14,83 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.thephoenixit.mygymclub.R;
 
 import net.cachapa.expandablelayout.ExpandableLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-private ExpandableLayout expandable_layout1;
+    private ExpandableLayout expandable_layout1, expandable_layout2;
+    private DrawerLayout drawer;
+    private LineChart lineWeightChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        lineWeightChart = findViewById(R.id.lineWeight);
+        lineWeightChart.setDrawGridBackground(false);
+        // no description text
+        lineWeightChart.getDescription().setEnabled(false);
+        lineWeightChart.animateX(2500);
 
+        LimitLine llXAxis = new LimitLine(10f, "Index 10");
+        llXAxis.setLineWidth(4f);
+        llXAxis.enableDashedLine(10f, 10f, 0f);
+        llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        llXAxis.setTextSize(10f);
+
+        XAxis xAxis = lineWeightChart.getXAxis();
+        xAxis.enableGridDashedLine(10f, 10f, 0f);
+
+        LimitLine ll1 = new LimitLine(150f, "Upper Limit");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        ll1.setTextSize(10f);
+
+        LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        ll2.setTextSize(10f);
+        YAxis leftAxis = lineWeightChart.getAxisLeft();
+        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+        leftAxis.addLimitLine(ll1);
+        leftAxis.addLimitLine(ll2);
+        leftAxis.setAxisMaximum(200f);
+        leftAxis.setAxisMinimum(-50f);
+        //leftAxis.setYOffset(20f);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawZeroLine(false);
+
+        // limit lines are drawn behind data (and not on top)
+        leftAxis.setDrawLimitLinesBehindData(true);
+
+        lineWeightChart.getAxisRight().setEnabled(false);
+        // add data
+        setChartData(45, 100);
+        Button btn_menu = findViewById(R.id.btn_menu);
+        btn_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawer.closeDrawer(GravityCompat.START);
+            }
+        });
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,18 +100,29 @@ private ExpandableLayout expandable_layout1;
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        TextView tv_weight = findViewById(R.id.tv_weight);
+        tv_weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (expandable_layout2.isExpanded()) {
+                    expandable_layout2.collapse();
+                } else {
+                    expandable_layout2.expand();
+                }
+            }
+        });
         TextView user_name = findViewById(R.id.user_name);
         TextView user_email = findViewById(R.id.user_email);
         TextView item_class_schedule = findViewById(R.id.item_class_schedule);
         user_name.setText("Hamza JGUERIM");
         user_email.setText("hamzajg@gmail.com");
         expandable_layout1 = findViewById(R.id.expandable_layout1);
+        expandable_layout2 = findViewById(R.id.expandable_layout2);
         item_class_schedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,6 +133,54 @@ private ExpandableLayout expandable_layout1;
                 }
             }
         });
+    }
+
+    private void setChartData(int count, float range) {
+        ArrayList<Entry> values = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+
+            float val = (float) (Math.random() * range) + 3;
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+
+        if (lineWeightChart.getData() != null &&
+                lineWeightChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) lineWeightChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            lineWeightChart.getData().notifyDataChanged();
+            lineWeightChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            set1.setDrawIcons(false);
+
+            // set the line to be drawn like this "- - - - - -"
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+            set1.setFillColor(Color.WHITE);
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData data = new LineData(dataSets);
+
+            // set data
+            lineWeightChart.setData(data);
+        }
     }
 
     @Override
@@ -84,12 +206,6 @@ private ExpandableLayout expandable_layout1;
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
